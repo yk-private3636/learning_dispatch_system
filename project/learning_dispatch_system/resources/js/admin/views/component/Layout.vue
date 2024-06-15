74c0e58b2fbe94edfa5e498a07eb95b7a0a77f95
yk-private3636 <hsnysk318@gmail.com>
2024-06-15 21:24:31 +0900

    redis導入、セッション管理をredisに変更、不要パッケージ削除

--- project/learning_dispatch_system/resources/js/admin/views/component/Layout.vue
+++ project/learning_dispatch_system/resources/js/admin/views/component/Layout.vue
@@ -139,7 +139,11 @@ const logoutExe = () => {
               />
             </svg>
             <span class="ms-3">{{ text.contact }}</span>
-            <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
+            <span
+              class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"
+            >
+              3
+            </span>
           </RouterLink>
         </li>
       </ul>
